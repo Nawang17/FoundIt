a4ab6e4885be37c5151da88fbd1c55054d74bbbf
Mahim Ali <117038967+Mahimali937@users.noreply.github.com>
2025-09-28 23:36:29 -0400

    Added initial user profile implementation

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -47,7 +47,7 @@ export default function ProfilePage() {
                 <Center>
                     <Box ta="center">
                         <Text fw={700} size="28px" style={{ letterSpacing: "-0.3px" }}>
-                            {user?.displayName || "Welcome to your profile!"}
+                            {user?.displayName || "Welcome!"}
                         </Text>
                         <Text c="dimmed" size="md" mb="lg">
                             {user?.email || ""}
